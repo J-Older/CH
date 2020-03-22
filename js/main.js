92dc8d4efd12c1a5a32413b1fa19739aa49969e7
J-Older <48174604+J-Older@users.noreply.github.com>
2020-03-22 19:30:54 +0200

    CH

---
+++ js/main.js
@@ -0,0 +1,146 @@
+
+$('.menu-toggle').click(function () {
+
+    var links = $('.menu-links');
+    var blur = $('.section');
+    var status = $('.line-status');
+    var menu = $('.menu-left');
+
+
+    $('ul').toggleClass('opening');
+    $(blur).attr('style') ?
+        $(blur).removeAttr('style') :
+        $(blur).attr('style',
+            'filter: blur(10px);' +
+            'position: fixed !important;' +
+            'transform: scale(1.5);'
+        );
+
+    $(links).attr('style') ?
+        $(links).removeAttr('style') :
+        $(links).attr('style', 'visibility: hidden;');
+
+    $(status).attr('style') ?
+        $(status).removeAttr('style') :
+        $(status).attr('style', 'visibility: hidden;');
+
+    $(menu).attr('style') ?
+        $(menu).removeAttr('style') :
+        $(menu).attr('style', 'width: 100vw;');
+
+
+    $(this).toggleClass('open');
+});
+
+
+$(function () {
+    var w = $(window).width(),
+        wrapper = $('.wrapper'),
+        progress = $('.progress'),
+        main = $('main'),
+        aw = $('nav a').width(),
+        navWidth = $('nav').width();
+
+    $(aw).on('click', function (e) {
+        var $anchor = $(this);
+        main.stop().animate({
+            scrollLeft: $anchor.data('href') * w - w
+        }, 500);
+        e.preventDefault();
+    });
+
+    $('#right').on('mousewheel DOMMouseScroll', function (e) {
+        var delta = Math.max(-1, Math.min(1, (e.originalEvent.wheelDelta || -e.originalEvent.detail)));
+        var left = wrapper.offset().left,
+            scrollLeft = delta > 0 ? (left + w) * -delta : (left - w) * delta;
+        main.stop().animate({
+            scrollLeft: scrollLeft
+        }, 500);
+    });
+
+    main.on('scroll', function () {
+        var left = wrapper.offset().left,
+            w = -navWidth / wrapper.width() * left;
+        progress.css('width', w + 'px');
+        console.log(Math.round(w / aw));
+        $('nav a').removeClass('active');
+        for (var i = Math.round(w / aw) + 1; i > 1; i--) {
+            console.log(i);
+            $('[data-href=' + i + ']').addClass('active');
+        }
+    });
+});
+
+
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm = $preloader.find('.svg_anm');
+    // $svg_anm.fadeOut();
+    $preloader.delay(1000).fadeOut('slow');
+});
+
+
+$('#mode').change(function () {
+
+    var menu = $('.menu-left');
+    var item = $('.screen-item');
+    var shadow = $('.shadow');
+
+    if ($(this).prop('checked')) {
+        $('body').addClass('dark-mode');
+        $(menu).addClass('dark-mode');
+        $(menu).addClass('dark-mode-out');
+        $('ul').addClass('dark-mode');
+        $(item).addClass('dark-mode');
+        $(item).addClass('dark-mode-out');
+        $(shadow).addClass('dark-mode-shadow');
+    } else {
+        $('body').removeClass('dark-mode');
+        $(menu).removeClass('dark-mode');
+        $(menu).removeClass('dark-mode-out');
+        $('ul').removeClass('dark-mode');
+        $(item).removeClass('dark-mode');
+        $(item).removeClass('dark-mode-out');
+        $(shadow).removeClass('dark-mode-shadow');
+    }
+
+});
+
+
+$(function () {
+    var menu = $('.menu-left');
+    var item = $('.screen-item');
+    var shadow = $('.shadow');
+    var data = localStorage.getItem("showning");
+
+    if (data !== null) {
+        $('body').addClass('dark-mode');
+        $(menu).addClass('dark-mode');
+        $(menu).addClass('dark-mode-out');
+        $('ul').addClass('dark-mode');
+        $(item).addClass('dark-mode');
+        $(item).addClass('dark-mode-out');
+        $(shadow).addClass('dark-mode-shadow');
+        $("input[name='switch_show']").attr("checked", "checked");
+    } else {
+        $('body').removeClass('dark-mode');
+        $(menu).removeClass('dark-mode');
+        $(menu).removeClass('dark-mode-out');
+        $('ul').removeClass('dark-mode');
+        $(item).removeClass('dark-mode');
+        $(item).removeClass('dark-mode-out');
+        $(shadow).removeClass('dark-mode-shadow');
+    }
+});
+
+
+$("input[name='switch_show']").click(function () {
+
+    if ($(this).is(":checked")) {
+
+        localStorage.setItem("showning", 1);
+    } else {
+        localStorage.removeItem("showning");
+    }
+
+});
